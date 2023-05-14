466ca516f40ce71d06ca61c78526c5f9053d15e4
Michal Haj <michalhaj2003@gmail.com>
2023-05-14 13:18:56 +0200

    speed up build process

--- next.config.mjs
+++ next.config.mjs
@@ -18,12 +18,12 @@ const config = {
     locales: ["en"],
     defaultLocale: "en",
   },
-  // typescript: {
-  //   ignoreBuildErrors: true,
-  // },
-  // eslint: {
-  //   ignoreDuringBuilds: true,
-  // },
-  // swcMinify: true,
+  typescript: {
+    ignoreBuildErrors: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+  swcMinify: true,
 };
 export default config;
